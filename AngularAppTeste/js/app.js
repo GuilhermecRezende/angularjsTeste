683890a999b558057ed18c91992cba3c4a46ae66
gcamilor <gcamilor@gmail.com>
2014-11-05 14:07:25 -0200

    First

---
+++ AngularAppTeste/js/app.js
@@ -0,0 +1,19 @@
+﻿var myApp = angular.module('app', ['ui.router']);
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+
+        .state('home', {
+        	url: '/',
+        	templateUrl: "templates/home.html"
+        })
+
+		.state('about', {
+			url: '/about',
+			templateUrl: "templates/about.html"
+		})
+
+});
